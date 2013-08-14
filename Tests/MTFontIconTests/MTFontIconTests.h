a4e9f98b8904a07b2cce14ae08d9b8344f947b1c
Gio <giovanni.lodi42@gmail.com>
2013-08-14 10:20:47 +0200

    Added unit test target

---
+++ Tests/MTFontIconTests/MTFontIconTests.h
@@ -0,0 +1,13 @@
+//
+//  MTFontIconTests.h
+//  MTFontIconTests
+//
+//  Created by Gio on 14/08/2013.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MTFontIconTests : SenTestCase
+
+@end
