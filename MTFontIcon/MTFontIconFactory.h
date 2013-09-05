240ad2f161120cee1be2a11fd025c67908065944
Gio <giovanni.lodi42@gmail.com>
2013-09-05 23:38:33 +0100

    Added a note in the header of the icons factory

--- MTFontIcon/MTFontIconFactory.h
+++ MTFontIcon/MTFontIconFactory.h
@@ -8,15 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
-// TODO maybe move in it's own file?
-@interface MTFontIconView : UIView
-@property (nonatomic, strong) UIColor *color;
-@property (nonatomic, strong) UIColor *shadowColor;
-@property (nonatomic, assign) CGSize shadowOffset;
-@end
+@class MTFontIconView;
 
 @interface MTFontIconFactory : NSObject
 
 - (MTFontIconView *)iconViewForIconNamed:(NSString *)iconName withSide:(CGFloat)side;
 
+@end
+
+// MTFontIconView is kept here with MTFontIconFactory because it's not intended to be used by itself.
+@interface MTFontIconView : UIView
+@property (nonatomic, strong) UIColor *color;
+@property (nonatomic, strong) UIColor *shadowColor;
+@property (nonatomic, assign) CGSize shadowOffset;
 @end
