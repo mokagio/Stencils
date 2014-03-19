961ff04e42571a866cdb4ec58eccc6f413764eee
Gio <giovanni.lodi42@gmail.com>
2014-03-19 22:15:25 +0000

    Added steppers for scale adjustement and proprotional offset top

--- IconSettings/IconSettings/MainViewController.m
+++ IconSettings/IconSettings/MainViewController.m
@@ -17,6 +17,13 @@
 
 @property (nonatomic, strong) UILabel *baselineAdjustementLabel;
 @property (nonatomic, strong) UIStepper *baselineAdjustementStepper;
+
+@property (nonatomic, strong) UILabel *scaleAdjustementLabel;
+@property (nonatomic, strong) UIStepper *scaleAdjustementStepper;
+
+@property (nonatomic, strong) UILabel *offsetTopLabel;
+@property (nonatomic, strong) UIStepper *offsetTopStepper;
+
 @end
 
 @implementation MainViewController
@@ -50,6 +57,35 @@
     self.baselineAdjustementLabel.textColor = [UIColor whiteColor];
     self.baselineAdjustementLabel.adjustsFontSizeToFitWidth = YES;
     [self.view addSubview:self.baselineAdjustementLabel];
+    
+    self.scaleAdjustementStepper = [[UIStepper alloc] init];
+    self.scaleAdjustementStepper.value = 1.0;
+    self.scaleAdjustementStepper.stepValue = 0.05;
+    [self.scaleAdjustementStepper addTarget:self action:@selector(reloadIcon) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:self.scaleAdjustementStepper];
+    
+    self.scaleAdjustementLabel = [[UILabel alloc] initWithFrame:CGRectMake(0,
+                                                                           0,
+                                                                           self.view.frame.size.width - 40 - self.scaleAdjustementStepper.frame.size.width,
+                                                                           self.scaleAdjustementStepper.frame.size.height)];
+    self.scaleAdjustementLabel.textColor = [UIColor whiteColor];
+    self.scaleAdjustementLabel.adjustsFontSizeToFitWidth = YES;
+    [self.view addSubview:self.scaleAdjustementLabel];
+    
+    self.offsetTopStepper = [[UIStepper alloc] init];
+    self.offsetTopStepper.value = 0;
+    self.offsetTopStepper.stepValue = 0.01;
+    self.offsetTopStepper.minimumValue = -1;
+    [self.offsetTopStepper addTarget:self action:@selector(reloadIcon) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:self.offsetTopStepper];
+    
+    self.offsetTopLabel = [[UILabel alloc] initWithFrame:CGRectMake(0,
+                                                                    0,
+                                                                    self.view.frame.size.width - 40 - self.offsetTopStepper.frame.size.width,
+                                                                    self.offsetTopStepper.frame.size.height)];
+    self.offsetTopLabel.textColor = [UIColor whiteColor];
+    self.offsetTopLabel.adjustsFontSizeToFitWidth = YES;
+    [self.view addSubview:self.offsetTopLabel];
 }
 
 - (void)viewDidLayoutSubviews
@@ -68,6 +104,26 @@
     baselineAdjustementStepperFrame.origin.y = self.baselineAdjustementLabel.frame.origin.y;
     baselineAdjustementStepperFrame.origin.x = CGRectGetMaxX(self.baselineAdjustementLabel.frame);
     self.baselineAdjustementStepper.frame = baselineAdjustementStepperFrame;
+    
+    CGRect scaleAdjustementLabelFrame = self.scaleAdjustementLabel.frame;
+    scaleAdjustementLabelFrame.origin.y = CGRectGetMaxY(self.baselineAdjustementLabel.frame) + 10;
+    scaleAdjustementLabelFrame.origin.x = 20;
+    self.scaleAdjustementLabel.frame = scaleAdjustementLabelFrame;
+    
+    CGRect scaleAdjustementStepperFrame = self.scaleAdjustementStepper.frame;
+    scaleAdjustementStepperFrame.origin.y = self.scaleAdjustementLabel.frame.origin.y;
+    scaleAdjustementStepperFrame.origin.x = CGRectGetMaxX(self.scaleAdjustementLabel.frame);
+    self.scaleAdjustementStepper.frame = scaleAdjustementStepperFrame;
+    
+    CGRect offsetTopLabelFrame = self.offsetTopLabel.frame;
+    offsetTopLabelFrame.origin.y = CGRectGetMaxY(self.scaleAdjustementLabel.frame) + 10;
+    offsetTopLabelFrame.origin.x = 20;
+    self.offsetTopLabel.frame = offsetTopLabelFrame;
+    
+    CGRect offsetTopStepperFrame = self.offsetTopStepper.frame;
+    offsetTopStepperFrame.origin.y = self.offsetTopLabel.frame.origin.y;
+    offsetTopStepperFrame.origin.x = CGRectGetMaxX(self.offsetTopLabel.frame);
+    self.offsetTopStepper.frame = offsetTopStepperFrame;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -82,11 +138,13 @@
 - (void)reloadIcon
 {
     self.icon.baselineAdjustement = self.baselineAdjustementStepper.value;
-    self.icon.scaleAdjustement = 1.0;
-    self.icon.proportionalOffsetTop = 0;
+    self.icon.scaleAdjustement = self.scaleAdjustementStepper.value;;
+    self.icon.proportionalOffsetTop = self.offsetTopStepper.value;
     [self.icon setNeedsLayout];
     
     self.baselineAdjustementLabel.text = [NSString stringWithFormat:@"baseline adjustement: %.2f", self.baselineAdjustementStepper.value];
+    self.scaleAdjustementLabel.text = [NSString stringWithFormat:@"scale adjustement: %.2f", self.scaleAdjustementStepper.value];
+    self.offsetTopLabel.text = [NSString stringWithFormat:@"proportional offset top: %.2f", self.offsetTopStepper.value];
 }
 
 #pragma mark - Status Bar
